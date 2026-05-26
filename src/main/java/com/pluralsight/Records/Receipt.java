24fac42da4c1675b79972e33793550035c0fb891
Joy-ConScion <chris.torres2001@outlook.com>
2026-05-26 10:38:20 -0500

    Add receipt class for receipt manager along with bufferedwriter (to create a csv) and then a reader to access later

---
+++ src/main/java/com/pluralsight/Records/Receipt.java
@@ -0,0 +1,65 @@
+package com.pluralsight.Records;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Receipt {
+
+    private LocalDate date;
+    private LocalTime time;
+    private String description;
+    private String vendor;
+    private double total;
+
+    public Receipt(LocalDate date, LocalTime time, String description, String vendor, double total) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.total = total;
+    }
+
+    public Receipt() {
+
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+}
--- src/main/java/com/pluralsight/Records/ReceiptManager.java
+++ src/main/java/com/pluralsight/Records/ReceiptManager.java
@@ -1,4 +1,71 @@
 package com.pluralsight.Records;
 
+import java.io.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
 public class ReceiptManager {
+
+    LocalDateTime now = LocalDateTime.now();
+    String date = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    String time = now.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+    static ArrayList<Receipt> receipts = new ArrayList<>();
+
+    public static void writeAction(String formatDate1, String formatTime2, String description, String vendor, double total) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("ReceiptLog.csv", true))) {
+            bufferedWriter.write(formatDate1 + "|" + formatTime2 + "|" + description + "|" + vendor + "|" + total);
+            bufferedWriter.newLine();
+
+            Receipt currentReceipt = new Receipt(
+                    LocalDate.parse(formatDate1),
+                    LocalTime.parse(formatTime2),
+                    description,
+                    vendor,
+                    total
+            );
+
+            currentReceipt.setDate(LocalDate.parse(formatDate1));
+            currentReceipt.setTime(LocalTime.parse(formatTime2));
+            currentReceipt.setDescription(description);
+            currentReceipt.setVendor(vendor);
+            currentReceipt.setTotal(total);
+            receipts.add(currentReceipt);
+
+        } catch (IOException e) {
+            System.out.println("Unexpected Error | Entry could not be stored");
+        }
+    }
+
+    public static ArrayList<Receipt> fetchReceiptLog() {
+        receipts.clear();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("ReceiptLog.csv"))) {
+
+            String input;
+            DateTimeFormatter formatter1 =
+                    DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            DateTimeFormatter formatter2 =
+                    DateTimeFormatter.ofPattern("HH:mm:ss");
+
+            while ((input = bufferedReader.readLine()) != null) {
+                String[] tokAttrib = input.split("\\|");
+
+                Receipt currentReceipt = new Receipt();
+
+                currentReceipt.setDate(LocalDate.parse(tokAttrib[0], formatter1));
+                currentReceipt.setTime(LocalTime.parse(tokAttrib[1], formatter2));
+                currentReceipt.setDescription(tokAttrib[2]);
+                currentReceipt.setVendor(tokAttrib[3]);
+                currentReceipt.setTotal(Double.parseDouble(tokAttrib[4]));
+                receipts.add(currentReceipt);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error - Couldn't retrieve request");
+            e.printStackTrace();
+        }
+        return receipts;
+    }
 }
