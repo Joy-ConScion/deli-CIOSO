1ae9bc614d44f3c7ccb32f492cf08c13709e819a
Joy-ConScion <chris.torres2001@outlook.com>
2026-05-28 22:20:56 -0500

    Add foundation code for BLT

---
+++ src/main/java/com/pluralsight/model/BLT.java
@@ -0,0 +1,17 @@
+package com.pluralsight.model;
+
+public class BLT extends Sandwich{
+
+    public BLT() {
+        super("IDK generic", 0.00);
+        this.size = "8' in";
+        this.bread = "White";
+        this.meat = "Bacon";
+        this.topping = "Lettuce";
+        this.cheese = "Cheddar";
+        this.sauce = "Ranch";
+        this.toasted = true;
+    }
+
+
+}
